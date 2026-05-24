3ace450efd32857b20503c625ac08e27417f6f42
mgerrit2 <gerrits.marc@hotmail.com>
2026-05-24 21:05:59 +0200

    first setup

---
+++ src/main/java/com/example/security_entities_common/SecurityEntitiesCommonApplication.java
@@ -0,0 +1,13 @@
+package com.example.security_entities_common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityEntitiesCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityEntitiesCommonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/security_entities_common/SecurityEntitiesCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.security_entities_common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityEntitiesCommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
